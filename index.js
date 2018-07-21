810da3d03f3833c3adccfb31a0edcd86ac38fac2
Julio Sepia <jsepia@juliosepia.com>
2018-07-21 13:13:23 -0700

    More fixes

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-const fs = require('fs')
-const winston = require('winston')
-const app = require('./app')
+var lambdaExpress = require('lambda-express')
+var app           = require('./app')
 
-const SCRIPTS_PATH = './lib/scripts'
-const SCRIPT_REGEX = /\.js$/gi
-
-winston.level = process.env.LOG_LEVEL || 'info'
-
-function loadScripts() {
-  winston.debug(`Looking for scripts in '${SCRIPTS_PATH}'...`)
-  const files = fs.readdirSync(SCRIPTS_PATH)
-
-  winston.debug(`${files.length} files found`)
-  for (let i = 0, l = files.length; i < l; i++) {
-    if (files[i].match(SCRIPT_REGEX)) {
-      const scriptPath = `${SCRIPTS_PATH}/${files[i]}`
-      winston.debug(`Found script ${files[i]} at ${scriptPath}`)
-      const script = require(scriptPath)
-      app.registerScript(script.endpoint, script.requestHandler)
-    }
-    else {
-      winston.debug(`${files[i]} is not a javascript file`)
-    }
-  }
-}
-
-loadScripts()
-
-module.exports = app.scripts
+// or, if you want to do some preprocessing before delegating, pass a function in:
+exports.handler = lambdaExpress.appHandler(function(event, context) { // eslint-disable-line no-unused-vars
+  return app
+})
