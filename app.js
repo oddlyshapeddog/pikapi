3b94abccf8676bfda04c57ce8699c38037fc0db8
Julio Sepia <jsepia@juliosepia.com>
2018-07-21 13:12:00 -0700

    Attempt to implement express

--- app.js
+++ app.js
@@ -1,75 +1,9 @@
-const winston = require('winston')
+const express = require('express')
+const app = express()
 
-const scripts = {}
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
 
-function registerScript(endpoint, requestHandler) {
-  winston.debug(`Setting up ${endpoint} request handler`)
-  scripts[endpoint] = (event, context, callback) => {
-    handleEvent(endpoint, event, requestHandler, callback)
-  }
-}
+module.exports = app
 
-function validateEvent(event) {
-  if (!event) {
-    throw new Error(`No event specified`)
-  }
-
-  if (!event.queryStringParameters) {
-    event = {
-      queryStringParameters: event
-    }
-  }
-
-  return event
-}
-
-function handleEvent(route, event, handler, callback) {
-  function sendSuccessResponse(body) {
-    const bodyString = body.toString()
-    winston.info(`[RESPONSE] ${bodyString}`)
-    callback(
-      null,
-      buildResponse(
-        200,
-        bodyString
-      )
-    )
-  }
-
-  function sendErrorResponse(error) {
-    const errorString = error.toString()
-    winston.error(errorString)
-    callback(
-      null,
-      buildResponse(
-        200,
-        `Error: ${errorString}` || `Unknown error occurred while handling request ${JSON.parse(event)}`
-      )
-    )
-  }
-
-  winston.info(`GET /${route} ${JSON.stringify(event)}`)
-  try {
-    validateEvent(event)
-    handler(event.queryStringParameters)
-      .then(
-        sendSuccessResponse,
-        sendErrorResponse
-      )
-  } catch(e) {
-    sendErrorResponse(e)
-  }
-}
-
-function buildResponse(statusCode, body) {
-  return {
-    statusCode: statusCode,
-    headers: { 'Content-Type': 'text/plain' },
-    body: body
-  }
-}
-
-module.exports = {
-  registerScript: registerScript,
-  scripts: scripts
-}
---
+++ index.old.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const winston = require('winston')
+const app = require('./app')
+
+const SCRIPTS_PATH = './lib/scripts'
+const SCRIPT_REGEX = /\.js$/gi
+
+winston.level = process.env.LOG_LEVEL || 'info'
+
+function loadScripts() {
+  winston.debug(`Looking for scripts in '${SCRIPTS_PATH}'...`)
+  const files = fs.readdirSync(SCRIPTS_PATH)
+
+  winston.debug(`${files.length} files found`)
+  for (let i = 0, l = files.length; i < l; i++) {
+    if (files[i].match(SCRIPT_REGEX)) {
+      const scriptPath = `${SCRIPTS_PATH}/${files[i]}`
+      winston.debug(`Found script ${files[i]} at ${scriptPath}`)
+      const script = require(scriptPath)
+      app.registerScript(script.endpoint, script.requestHandler)
+    }
+    else {
+      winston.debug(`${files[i]} is not a javascript file`)
+    }
+  }
+}
+
+loadScripts()
+
+module.exports = app.scripts
